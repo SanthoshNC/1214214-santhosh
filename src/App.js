22eaaf14b16b77803d5a0407e4e752ab7aa15ea1
SanthoshNC <ncsanthosh7@gmail.com>
2024-11-07 15:11:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World - Santhosh NC - 1214214
         </p>
         <a
           className="App-link"
